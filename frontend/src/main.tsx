2aec5da2954ffd67bc525a7b45297f2fbacb5227
Anushka Bhavsar <anushkabhavsar2000@gmail.com>
2025-06-15 22:17:06 +0530

    Routing Updated

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -3,8 +3,9 @@ import App from './App.tsx'
 import './index.css'
 import { BrowserRouter } from "react-router-dom";
 
-createRoot(document.getElementById("root")!).render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>
-);
+
+createRoot(document.getElementById("root")!).render(<App />);
+
+<BrowserRouter>
+  <App />
+</BrowserRouter>
